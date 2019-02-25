9ff1dcf895b489da9b5a19dac507852f77a7c63d
sowmiya s <sowmiyas@20cube.com>
2019-02-25 18:29:58 +0530

    Transporter Type added in DB

--- src/app/eaxis/distribution/manifest-list/manifest-general/manifest-general.controller.js
+++ src/app/eaxis/distribution/manifest-list/manifest-general/manifest-general.controller.js
@@ -66,8 +66,8 @@
             if (!ManifestGeneralCtrl.currentManifest.isNew)
                 generalOperation();
 
-            ManifestGeneralCtrl.ePage.Masters.TransporterTypeRadioBtn = 'Transportation'
-            ManifestGeneralCtrl.ePage.Masters.Transportation = true;
+            // ManifestGeneralCtrl.ePage.Masters.TransporterTypeRadioBtn = 'Transportation'
+            // ManifestGeneralCtrl.ePage.Masters.Transportation = true;
             ManifestGeneralCtrl.ePage.Masters.SelectRadioButton = SelectRadioButton;
 
             GetNewManifestAddress();
@@ -78,19 +78,9 @@
 
         function SelectRadioButton(value) {
             dmsManifestConfig.TransporterTypeValue = value;
-            if (value == "ExpressDelivery") {
-                ManifestGeneralCtrl.ePage.Masters.ExpressDelivery = true;
-                ManifestGeneralCtrl.ePage.Masters.Transportation = false;
-                ManifestGeneralCtrl.ePage.Masters.LastMileDelivery = false;
-            } else if (value == "LastMileDelivery") {
-                ManifestGeneralCtrl.ePage.Masters.LastMileDelivery = true;
-                ManifestGeneralCtrl.ePage.Masters.ExpressDelivery = false;
-                ManifestGeneralCtrl.ePage.Masters.Transportation = false;
-            } else if (value == "Transportation") {
-                ManifestGeneralCtrl.ePage.Masters.Transportation = true;
-                ManifestGeneralCtrl.ePage.Masters.LastMileDelivery = false;
-                ManifestGeneralCtrl.ePage.Masters.ExpressDelivery = false;
-            }
+            console.log(ManifestGeneralCtrl.ePage.Entities.Header.Data.TmsManifestHeader.TransporterType)
+            console.log(dmsManifestConfig.TransporterTypeValue);
+            ManifestGeneralCtrl.ePage.Entities.Header.Data.TmsManifestHeader.TransporterType = value;
         }
 
         function OnFieldValueChange(code) {
