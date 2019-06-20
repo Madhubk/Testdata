dca9e754c36d90b782fdf21d4c88a4bfb4ffef5d
sowmiya s <sowmiyas@20cube.com>
2019-06-20 15:12:37 +0530

    response of response in the update and insert of package list changes

--- src/app/eaxis/warehouse/wh-releases/packing-module/pick-packing-general/pick-packing-general.controller.js
+++ src/app/eaxis/warehouse/wh-releases/packing-module/pick-packing-general/pick-packing-general.controller.js
@@ -254,7 +254,7 @@
             apiService.post("eAxisAPI", PackingGeneralCtrl.ePage.Entities.Header.API.UpdatePackage.Url, PackingGeneralCtrl.ePage.Masters.Config.PackageListDetails[PackingGeneralCtrl.currentPick.label].PackageListDetails).then(function (response) {
                 if (response.data.Response) {
                     // Get By Id Call
-                    apiService.get("eAxisAPI", PackingGeneralCtrl.ePage.Entities.Header.API.PackageGetByID.Url + response.data.Response.Response.PK).then(function (response) {
+                    apiService.get("eAxisAPI", PackingGeneralCtrl.ePage.Entities.Header.API.PackageGetByID.Url + response.data.Response.PK).then(function (response) {
                         PackingGeneralCtrl.ePage.Masters.UpdateditemList = response.data.Response.lstUIPackageItems;
                         PackingGeneralCtrl.ePage.Masters.Config.PackageListDetails[PackingGeneralCtrl.currentPick.label] = {
                             PackageListDetails: response.data.Response
